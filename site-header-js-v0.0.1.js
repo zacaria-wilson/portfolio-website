5adad79609fc3c7c1fa0341865ade1659def16ba
zacaria-wilson <102315507+zacaria-wilson@users.noreply.github.com>
2024-10-11 16:12:25 -0400

    Updated Scrollbar Styles, Added Copy Email

--- site-header-js-v0.0.1.js
+++ site-header-js-v0.0.1.js
@@ -5,6 +5,22 @@
     function globalHeaderFunctions(){
         let metaEl = document.querySelector('[name ="viewport"]');
         metaEl.setAttribute('content', 'width=device-width, initial-scale=1, minimum-scale=1');
+
+        let copyEmailButtons = document.querySelectorAll('.copy-email');
+        copyEmailButtons.forEach(el =>{
+            el.addEventListener('click', (e)=>{
+                console.log('el is', el)
+                navigator.clipboard.writeText('zacaria.b.wilson@gmail.com');
+                el.innerText = 'Copied'
+                setTimeout(()=>{
+                    el.innerText = 'Copy'
+                }, 1000)
+            })
+        })
+
+        window.scrollToTop = ()=>{
+            window.scrollTo({top:0, behavior:'smooth'})
+        }
     }; 
 
     //Push this function to onload list
