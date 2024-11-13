006f11e93e9ebb4faa32e2781308d50cbd36c584
zacaria-wilson <102315507+zacaria-wilson@users.noreply.github.com>
2024-11-13 17:28:40 -0500

    Moderate Update: Fixes For Visual Errors
    - JS and CSS changes for line backgrounds on Home and Contact pages. Now using grid to align elements.
    
    - Changed Projects page JS and CSS to apply 'closed' class to slide elements, rather than description elements and adjusted styles. Needed for making making descriptions full-height when open while still allowing auto height layout when closed.
    
    - Fixed issue with navbar drop down menu not being full width, due to padding applied by parent elements. Needed for navbar to be placed within page content grid layouts, which is ideal for consistency.

---
+++ production/home-page-js-v.1.0.3.js
@@ -0,0 +1,34 @@
+function homepageAnimations() {
+    function onloadJS(){
+
+    function heroBGAnimation() {
+        let gridList = document.querySelectorAll('.bg-line-grid');
+        let intervalX = 3.33;
+        let intervalY = 3.33 * 0.5;
+
+        gridList.forEach((el, index) => {
+            el.style.gridTemplateColumns = `calc(${intervalX * index}%) var(--hero-bg-x-start-a) calc(var(--hero-bg-x-start-b) - ${intervalX * index}%)`
+            el.style.gridTemplateRows = `calc(var(--hero-bg-y-start) + ${intervalY * index}%) calc(100% - var(--hero-bg-y-start) - ${intervalY * index}%)`
+        });
+
+    };
+    
+    heroBGAnimation();
+    console.log('animations started')
+
+
+};
+
+function executeOnloadJS() {
+
+    if (window.addEventListener){window.addEventListener("load", onloadJS, false)}
+
+    else if (window.attachEvent){window.attachEvent("load", onloadJS)}
+
+    else {window.onload = onloadJS()}
+}
+
+executeOnloadJS();
+
+};
+homepageAnimations();
